f89dea1b7db5409e11ce6da1b11a913dca0f9b22
Cristian-costra <a01741850@tec.mx>
2024-11-07 20:40:42 -0600

    passed some methods from main branch to this

--- Activities/Act4.1/appGrafo.cpp
+++ Activities/Act4.1/appGrafo.cpp
@@ -1,46 +1,160 @@
 #include "..\..\DataStructures\Graph\Graph.hpp"
 #include "..\..\DataStructures\Graph\ArchPondGraph.hpp"
 
-/*/
-template <typename T, typename U>
-using vecGraphArchPond = ArchPondGraph<std::vector, std::vector, ArchPond<T,U>>; // Alias template 
-
-int main(){
-    vecGraph<std::pair<int, int>> god(3);
-    std::vector<std::vector<std::pair<int, double>>> 
-    container = {{{1, 4.0}, {2, 3.4}},
-                {{0, 5.6}, {2, 4.7}, {3, 6.8}},
-                {{0 , 10.4}, {1 , 30.2}, {4 , 9.6}},
-                {{1, 3.2} ,{4, 7.2}},
-                {{2, 21.2}, {3, 3.4}}};
-
-    ArchPondGraph<std::vector, std::vector, std::pair<int, double>> grx|x|aph2(container);
-
-    graph2.DFS(0);
-    std::cout<<std::endl;
-    graph2.BFS(0);
+template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
+void loadGraph(int n, int m, vertContainer<adjContainer<T>>& container){
+    if( n < 0 || m < 0 || m > n){
+        throw(std::invalid_argument("Valor de vértice inválido"));
+    }
     
+    container = vertContainer<adjContainer<T>>(); // Aquí hay que estandarizar el código para que funcione con otros que no sean vectores. Y no funcionará si no tiene un constructor con el tamaño como parámetros.
+    for(int i = 0; i < n; i++){
+        adjContainer<T> temp = adjContainer<T>();
+        for(int j = 0; j < m; j++){
+            temp.push_back(j);
+        }
+        container.push_back(temp); 
+    }
 }
 
-*/
+template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
+void DFS(vertContainer<adjContainer<T>>& container, int n){
+    if( n < 0 || n >= container.size()){
+        throw(std::invalid_argument("Valor de vértice inválido"));
+    }
+
+    std::cout<<"DFS ("<<n<<"): ";
+    std::vector<bool> visited(container.size(), false);
+    std::stack<int> stack;
+
+    stack.push(n);
+    visited[n] = true;
+
+    while(!stack.empty()){
+        n = stack.top();
+        std::cout << n << " ";
+        stack.pop();
+        for(const auto& element:container[n]){
+            if(!visited[element]){
+                visited[element] = true;
+                stack.push(element);
+            }
+        }
+    }
+    std::cout<<"\n";
+}
+
+
+template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
+void BFS(vertContainer<adjContainer<T>>& container, int n){
+    std::cout<<"\ndep: "<<n<<" "<<container.size();
+    if(n < 0 || n >= container.size()){
+        throw(std::out_of_range("El índice se encuentra fuera de rango"));
+    }
+
+    std::vector<bool> visited(container.size(), false);
+    std::queue<int> queue;
+    std::cout<<"BFS ("<<n<<"): ";
+    visited[n] = true;
+    queue.push(n);
+    while(!queue.empty()){
+        n = queue.front();
+        std::cout<<n<<" ";
+        queue.pop();
+        for(const auto& element:container[n]){
+            if(!visited[element]){
+                queue.push(element);
+                visited[element] = true;
+            }
+        }
+    }
+    std::cout<<"\n";
+}
 
 template <typename T>
 using vecGraph = Graph<std::vector, std::vector, T>; // Alias template 
 
 int main(){
-    vecGraph<int> god(3);
-    std::vector<std::vector<int>> container = {{1,2},{0,2,3},{0,1,4},{1,4},{2,3}};
-    Graph<> graph2(container);
+    vecGraph<int> testConstInt(3), testAddEdgeInt;
+    vecGraph<std::string> testConstStr(6), testAddEdgeStr;
+    testConstStr.print();
 
-    god.addEdge(1, 2);
-    god.print();
-    god.addEdge(2,2);
-    god.print();
-    god.removeEdge(1,2);
-    god.print();
-    god.DFS(0);
-    god.DFS(1);
-    graph2.DFS(0);
-    graph2.BFS(0);
+    // CASOS DE PRUEBA GENERALES DE LA IMPLEMENTACIÓN DE LA LISTA DE ADYACENCIA:
+    std::cout<<"> Impresión de la lista de adyacencia (añadiendo un arco de 1 a 2 cuando no existen ninguno de los vértices): \n";
+    try{
+        testAddEdgeInt.addEdge(1, 2);
+    } catch(std::out_of_range& ex){
+        std::cout<<"Error al intentar añadir arco: " <<ex.what()<<"\n";
+        testAddEdgeInt.print();
+    }
+
+    std::cout<<"\n> Impresión de la lista de adyacencia (añadiendo un arco de 1 a 2 cuando no existe vértice 2): \n";
+    testAddEdgeInt.addVertex(2);     // Añadir dos vértices, 0 y 1. 
+    try{
+        testAddEdgeInt.addEdge(1, 2);
+    } catch(std::out_of_range& ex){
+        std::cout<<"Error al intentar añadir arco: " <<ex.what()<<"\n";
+        testAddEdgeInt.print();
+    }
+
+    std::cout<<"\n> Impresión de la lista de adyacencia (añadiendo un arco de 2 a 2): \n";
+    testAddEdgeInt.addVertex(); // Añadir vértice 2
+    testAddEdgeInt.addEdge(1,2);
+    testAddEdgeInt.addEdge(2,2);
+    testAddEdgeInt.print();
+
+    std::cout<<"\n> Impresión de la lista de adyacencia (quitando el arco inexistente de 1 a 1): \n";
+    try{
+        testAddEdgeInt.removeEdge(1,1);
+    } catch(std::out_of_range& ex){
+        std::cout<<"Error al intentar quitar el arco: "<<ex.what()<<"\n";
+        testAddEdgeInt.print();
+    }
+
+    std::vector<std::vector<int>> container = {{1,2},{0,2,3},{0,1,4},{1,4},{2,3}};
+    Graph<> testGraphContainer(container), testLoadGraph;
+    std::cout<<"\n> Impresión de la lista de adyacencia creada con un contenedor como parámetro: \n";
+    testGraphContainer.print();
+    
+    std::cout<<"\n> Recorridos DFS: \n";
+    DFS(testGraphContainer.adjacencyList, 0);
+    DFS(testGraphContainer.adjacencyList, 1);
+    
+    std::cout<<"\n> Recorridos BFS: \n";
+    BFS(testGraphContainer.adjacencyList, 0);
+    BFS(testGraphContainer.adjacencyList, 1);
+    
+    std::cout<<"\n> Recorridos DFS y BFS en grafos vacíos: \n";
+    try{
+        DFS(testLoadGraph.adjacencyList, 0);
+    } catch(std::exception& ex){
+        std::cout<<"Error: "<<ex.what()<<"\n";
+    }
+
+    try{
+        BFS(testLoadGraph.adjacencyList, 0);
+    } catch(std::exception& ex){
+        std::cout<<"Error: "<<ex.what()<<"\n";
+    }
+
+    std::cout<<"\n> Implementación del método loadGraph con una lista de adyacencias como parámetro: \n";
+    testLoadGraph.loadGraph(container);
+    testLoadGraph.print();
+
+    std::cout<<"\n> Implementación del método loadGraph con valores inválidos: \n";
+    try{
+        testLoadGraph.loadGraph(-2,3);
+    } catch(std::invalid_argument& ex){
+        std::cout<<"Error al intentar cargar grafo: "<<ex.what()<<"\n";
+    }
+    std::cout<<"\n> LoadGraph con más conexiones que vértices (imposible): \n";
+    try{
+        testLoadGraph.loadGraph(4,5);
+    } catch(std::invalid_argument& ex){
+        std::cout<<"Error al intentar cargar grafo: "<<ex.what()<<"\n";
+    }
+    std::cout<<"\n> LoadGraph con más vértices que conexiones (correcto): \n";
+    loadGraph(5,4, testLoadGraph.adjacencyList);
+    testLoadGraph.print();
 }
 
--- DataStructures/Graph/GraphCopy.hpp
+++ DataStructures/Graph/GraphCopy.hpp
@@ -17,15 +17,15 @@ class Graph{
 
     private:
         bool directed;
-        // container adjacencyList;
     public:
         container adjacencyList;
 
-        Graph();
         Graph(bool _directed = true);
         Graph(int v, bool _directed = true);
         Graph(const container& adjList, bool _directed = true);
         Graph(const Graph& otherGraph, bool _directed = true);
+
+        ~Graph();
         
         const Graph& operator=(const Graph& otherGraph);
 
@@ -33,10 +33,11 @@ class Graph{
         void loadGraph(const container& adjList);
 
         void addEdge(int vertex, int connection);
-        void addVertex();
+        void addVertex(int n = 1);
         void addVertex(const adjContainer<T>& adjacency);
 
-        int hasEdge(int vertex, int connection, iterator_type& it);
+        int hasEdge(int vertex, int connection, iterator_type& it); // Existe solo para cuando, de existir el arco, se realice una operación con este. 
+        int hasEdge(int vertex, int connection);
         void removeEdge(int vertex0, int vertex1);
 
         void DFS(int index);
@@ -45,191 +46,138 @@ class Graph{
 
         bool empty();
         void clear();
+
+        bool isTree();
+        bool bipartiteGraph();
+        void topologicalSort();
 };
 
-/*
-PARAMETROS: void.
-MÉTODO: Constructor por defecto. Inicializa la lista de adyacencia.
-ORDER: O(1)
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
-Graph<vertContainer, adjContainer, T>::Graph(){
-    adjacencyList = *new container(); // Al parecer los contenedores de la STL se inicializan automáticamente, así que no es necesario poner new. Además, como son atributos de la clase, sobrevivirán al salir de ámbito. A menos que se tengan punteros como atributos. Pero como es sabido, en funciones, los datos inicializados de cualquier tipo no sobrevivirán al salir del ámbito a menos que se utilice el operador new. 
+Graph<vertContainer, adjContainer, T>::Graph(bool _directed):directed(_directed){
+    adjacencyList = container(); // Al parecer los contenedores de la STL se inicializan automáticamente, así que no es necesario poner new. Además, como son atributos de la clase, sobrevivirán al salir de ámbito. A menos que se tengan punteros como atributos. Pero como es sabido, en funciones, los datos inicializados de cualquier tipo no sobrevivirán al salir del ámbito a menos que se utilice el operador new. 
 }
 
-/*
-PARAMETROS: const container& adjList, es una lista de adyacencia que se utilizará para inicializar la lista de adyacencia del grafo.
-            bool _directed, indica si el grafo es dirigido o no.
-MÉTODO: Se inicializa la lista de adyacencia con la lista de adyacencia adjList.
-ORDER: O(V+E), donde V es el número de vértices y E es el número de aristas en adjList.
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 Graph<vertContainer, adjContainer, T>::Graph(const container& adjList, bool _directed):directed(_directed){
-    adjacencyList = *new container(adjList);  // ESTO CONFÍA EN QUE LA ESTRUCTURA DE DATOS UTILIZADA TIENE SOBRECARGADO EL OPERADOR DE ASIGNACIÓN.
+    adjacencyList = container(adjList);  // ESTO CONFÍA EN QUE LA ESTRUCTURA DE DATOS UTILIZADA TIENE SOBRECARGADO EL OPERADOR DE ASIGNACIÓN.
 }
 
-/*
-PARAMETROS: const Graph& otherGraph, otro grafo que se utilizará para inicializar la lista de adyacencia del grafo.
-            bool _directed, indica si el grafo es dirigido o no.
-MÉTODO: Se inicializa una nueva lista de adyacencia con la lista de adyacencia de otherGraph.
-ORDER: O(V+E), donde V es el número de vértices y E es el número de aristas en otherGraph.
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 Graph<vertContainer, adjContainer, T>::Graph(const Graph& otherGraph, bool _directed):directed(_directed){
-    delete adjacencyList; 
-    adjacencyList = *new container(otherGraph.adjacencyList);  // ESTO CONFÍA EN QUE LA ESTRUCTURA DE DATOS UTILIZADA TIENE SOBRECARGADO EL OPERADOR DE ASIGNACIÓN.
+    
+    adjacencyList = container(otherGraph.adjacencyList);  // ESTO CONFÍA EN QUE LA ESTRUCTURA DE DATOS UTILIZADA TIENE SOBRECARGADO EL OPERADOR DE ASIGNACIÓN.
 }
 
-/*
-PARAMETROS: const Graph& otherGraph, otro grafo que se utilizará para inicializar la lista de adyacencia del grafo.
-MÉTODO: Si existe una lista de adyacencia, la elimina, y la asigna a la lista de adyacencia de otherGraph a la del grafo.
-ORDER: O(V+E), donde V es el número de vértices y E es el número de aristas en otherGraph.
-RETORNA: const Graph<vertContainer, adjContainer, T>&, una referencia al grafo actual.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 const Graph<vertContainer, adjContainer, T>& Graph<vertContainer, adjContainer, T>::operator=(const Graph& otherGraph){
-    delete adjacencyList; 
-    adjacencyList = *new container(otherGraph.adjacencyList);  // ESTO CONFÍA EN QUE LA ESTRUCTURA DE DATOS UTILIZADA TIENE SOBRECARGADO EL OPERADOR DE ASIGNACIÓN.
+    adjacencyList = container(otherGraph.adjacencyList);  // ESTO CONFÍA EN QUE LA ESTRUCTURA DE DATOS UTILIZADA TIENE SOBRECARGADO EL OPERADOR DE ASIGNACIÓN.
 }
 
-/*
-PARAMETROS: int v, el número de vértices del grafo.
-            bool _directed, indica si el grafo es dirigido o no.
-MÉTODO: Crear un grafo con v vértices y sin aristas.
-ORDER: O(V), donde V es el número de vértices del grafo.
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 Graph<vertContainer, adjContainer, T>::Graph(int v, bool _directed): directed(_directed){
 
-    adjacencyList = *new container();
+    adjacencyList = container();
     for(int vx = 0; vx < v; vx++){
         adjacencyList.push_back(adjContainer<T>());
     }
 }
 
-/*
-PARAMETROS: int vertex, índice del vértice al que se le quiere agregar una arista.
-            int connection, índice del vértice al que se le quiere conectar el vértice.
-MÉTODO: Verifica si existe una arista entre vertex y connection. Si existe, se agrega una arista entre vertex.
-ORDER: O(E), donde E es el número de aristas en la lista de adyacencia de vertex.
-RETORNA: void.
-*/
+template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
+Graph<vertContainer, adjContainer, T>::~Graph(){} // los contenedores se borran cuando el objeto sale de ámbito. Como tal, los contenedores deberían tener su administración de la memoria.
+
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::addEdge(int vertex, int connection){
+    // Esto se podría incluir, pero en la implementación de un grafo, puede que se agreguen conexiones a vertices que aún no forman parte pero eventualmente lo harán. Arrojar una excepcion, consider que sería más pertinente hacerlo en un hasEdge. Tampoco creo factible el hecho de crear un nuevo vertice o n vertices hasta llegar al vertice que se busca conectar.
+    // Pero entonces, no le veo mucho sentido a un vector que no tenga los vertices completos hasta el vertice que quiere conectar. Pero el vertice de destino, considero yo, el error de no existir debería ser arrojado en otra función
+    
+    if(vertex < 0 || vertex >= adjacencyList.size() || connection < 0 || connection >= adjacencyList.size()){
+        throw(std::out_of_range("No existe tal vértice"));
+    }
+    
     iterator_type it;
     if (!hasEdge(vertex, connection, it)){
         adjacencyList[vertex].push_back(connection); // Esto asume que la estructura de datos utilizada tiene método push_back y tiene sobrecarga de []
     }
-    if(directed){
+    if(!directed){
         if (!hasEdge(connection, vertex, it)){
             adjacencyList[connection].push_back(vertex); 
         }
     }
 }
 
-/*
-PARAMETROS: int vertex, vertice al que se le quiere verificar si tiene una arista con connection.
-            int connection, vertice al que se le quiere verificar si tiene una arista con vertex.
-            iterator_type& it, vertice al que se le quiere verificar si tiene una arista con vertex.
-MÉTODO: find() verifica que existe connection en la lista de adyacencia de vertex.
-ORDER: O(E), donde E es el número de aristas en la lista de adyacencia de vertex.
-RETORNA: bool, true si existe una arista entre vertex y connection, false en otro caso.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 int Graph<vertContainer, adjContainer, T>::hasEdge(int vertex, int connection, iterator_type& it){
+    if(vertex < 0 || vertex >= adjacencyList.size() || connection < 0 || connection >= adjacencyList.size()){
+        throw(std::out_of_range("No existe tal vértice"));
+    }
+
     it = std::find(adjacencyList[vertex].begin(), adjacencyList[vertex].end(), connection);
     return it != adjacencyList[vertex].end();
 }
 
-/*
-PARAMETROS: void.
-MÉTODO: Agraga un vértice al final de la lista de adyacencia.
-ORDER: O(1)
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
-void Graph<vertContainer, adjContainer, T>::addVertex(){
-    adjacencyList.push_back(adjContainer<T>());
+int Graph<vertContainer, adjContainer, T>::hasEdge(int vertex, int connection){
+    if(vertex < 0 || vertex >= adjacencyList.size() || connection < 0 || connection >= adjacencyList.size()){
+        throw(std::out_of_range("No existe tal vértice"));
+    }
+    return std::find(adjacencyList[vertex].begin(), adjacencyList[vertex].end(), connection) != adjacencyList[vertex].end();
 }
 
-/*
-PARAMETROS: const adjContainer<T>& adjacency, vertice con su lista de adyacencia que se quiere agregar al grafo.
-MÉTODO: Agrega un vértice al final de la lista de adyacencia con sus aristas. Si es un grafo no dirigido, se agregan las aristas correspondientes.
-ORDER: O(E), donde E es el número de aristas en la lista de adyacencia de vertex.
-RETORNA: void.
-*/
+template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
+void Graph<vertContainer, adjContainer, T>::addVertex(int n){
+    if(n < 0){
+        throw(std::invalid_argument("Función de adición de vértices solo acepta valores positivos"));
+    }
+    for(int i = 0; i < n; i++){
+        adjacencyList.push_back(adjContainer<T>()); // Necesario poner el <T> después de adjContainer.
+    }
+}
+
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::addVertex(const adjContainer<T>& adjacency){
     adjacencyList.push_back(std::move(adjacency));
     if(!directed){
         for(const auto& element:adjacency){
-            addEdge(element, adjacencyList.size()-1);
+            addEdge(adjacencyList.size(), element);
         }
     }
 }
 
-/*
-PARAMETROS: int vertex0, vertice de origen de la arista.
-            int vertex1, vertice de destino de la arista.
-MÉTODO: Verifica si existe una arista entre vertex0 y vertex1. Si existe, se elimina la arista.
-ORDER: O(E), donde E es el número de aristas en la lista de adyacencia de vertex0.
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::removeEdge(int vertex0, int vertex1){
     iterator_type it;
     if(!hasEdge(vertex0, vertex1, it)){
-        throw(std::out_of_range("The graph does not have that edge"));
+        throw(std::out_of_range("El grafo no contiene tal arco"));
     }
     adjacencyList[vertex0].erase(it);
 }
 
-/*
-PARAMETROS: const container& adjList, lista de adyacencia que se utilizará para inicializar la lista de adyacencia del grafo.
-MÉTODO: Verifica si la lista de adyacencia es la misma que la lista de adyacencia del grafo.
-        Si no lo es, se elimina la lista de adyacencia y se asigna la lista de adyacencia adjList a la lista de adyacencia del grafo.
-ORDER: O(V+E), donde V es el número de vértices y E es el número de aristas en adjList.
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::loadGraph(const container& adjList){
-    if(*(this->adjacencyList) == &adjList){
+    if(&adjacencyList == &adjList){
         return;
     }
-    delete adjacencyList;
-
-    this -> adjacencyList = *new container(adjList);
+    adjacencyList = container(adjList);
 }
 
-/*
-PARAMETROS: int vertices, 
-            int connections, 
-MÉTODO: Se elimina la lista de adyacencia y se crea una nueva lista de adyacencia con vertices vértices y connections aristas.
-ORDER: 
-RETORNA: 
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::loadGraph(int vertices, int connections){
-    delete adjacencyList;
-    adjacencyList = *new container(); // Aquí hay que estandarizar el código para que funcione con otros que no sean vectores. Y no funcionará si no tiene un constructor con el tamaño como parámetros.
+    if( vertices < 0 || connections < 0 || connections > vertices){
+        throw(std::invalid_argument("Valor de vértice inválido"));
+    }
+    adjacencyList = container(); // Aquí hay que estandarizar el código para que funcione con otros que no sean vectores. Y no funcionará si no tiene un constructor con el tamaño como parámetros.
     for(int v = 0; v < vertices; v++){
-        adjacencyList.push_back(adjContainer<T>(connections)); 
+        adjContainer<T> temp = adjContainer<T>();
+        for(int i = 0; i < connections; i++){
+            temp.push_back(i);
+        }
+        adjacencyList.push_back(temp); 
     }
 }
 
-/*
-PARAMETROS: void.
-MÉTODO: Por cada vértice en la lista de adyacencia, se imprimen sus aristas. Si esta vacía, se imprime un corchete vacío.
-ORDER: O(V+E), donde V es el número de vértices del grafo y E es el número de aristas.
-RETORNA: void.
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::print(){
-    std::cout<<"Impresión de la lista de adyacencia: \n";
+    std::cout<<"[\n";
     int i = 0;
     for(const auto& vertVec:adjacencyList){
         std::cout <<i<<": ";
@@ -243,15 +191,9 @@ void Graph<vertContainer, adjContainer, T>::print(){
         }
         i++;
     }
-    std::cout<<"\n";
+    std::cout<<"]\n";
 }
 
-/*
-PARAMETROS: 
-MÉTODO: 
-ORDER: 
-RETORNA: 
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::BFS(int index){
     if(index < 0 || index >= adjacencyList.size()){
@@ -260,7 +202,7 @@ void Graph<vertContainer, adjContainer, T>::BFS(int index){
 
     std::vector<bool> visited(adjacencyList.size(), false);
     std::queue<int> queue;
-    std::cout<<"BFS: ";
+    std::cout<<"BFS ("<<index<<"): ";
     visited[index] = true;
     queue.push(index);
     while(!queue.empty()){
@@ -277,18 +219,12 @@ void Graph<vertContainer, adjContainer, T>::BFS(int index){
     std::cout<<"\n";
 }
 
-/*
-PARAMETROS: 
-MÉTODO: 
-ORDER: 
-RETORNA: 
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 void Graph<vertContainer, adjContainer, T>::DFS(int index){
-    if(index < 0 || index > adjacencyList.size()){
+    if(index < 0 || index >= adjacencyList.size()){
         throw(std::out_of_range("El índice se encuentra fuera de rango"));
     }
-    std::cout<<"DFS: ";
+    std::cout<<"DFS ("<<index<<"): ";
     std::vector<bool> visited(adjacencyList.size(), false);
     std::stack<int> stack;
 
@@ -309,14 +245,21 @@ void Graph<vertContainer, adjContainer, T>::DFS(int index){
     std::cout<<"\n";
 }
 
-/*
-PARAMETROS: 
-MÉTODO: 
-ORDER: 
-RETORNA: 
-*/
 template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
 bool Graph<vertContainer, adjContainer, T>::empty(){
     return adjacencyList.size() == 0;
 }
 
+template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
+void Graph<vertContainer, adjContainer, T>::clear(){
+    if(adjacencyList.size() == 0){
+        return;
+    }
+
+    adjacencyList = container(); // Esto es posible porque el contenedor, en su sobrecarga de asignación, ya tiene un proceso que elimina los datos actuales y cambia a los datos nuevos. No necesito eliminar el contenedor, dentro de su implementación se hace todo esto.
+}
+
+template <template <typename...> class vertContainer, template <typename...> class adjContainer, typename T>
+bool Graph<vertContainer, adjContainer, T>::isTree(){
+
+}
